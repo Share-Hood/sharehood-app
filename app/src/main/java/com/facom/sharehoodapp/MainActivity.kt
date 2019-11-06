59da3115f2d3e88ee8c3bbc8cd75646d8c235d97
Alison Moura <alison.oghino@gmail.com>
2019-11-05 20:47:28 -0400

    Primeiro commit - Criando página de login

---
+++ app/src/main/java/com/facom/sharehoodapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.facom.sharehoodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
