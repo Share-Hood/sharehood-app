969c728146104aa39d418b779657ab64497aa7d0
Kelli Regina <kellireigna3@hotmail.com>
2019-11-07 22:09:57 -0400

    validação login

--- app/src/main/java/com/facom/sharehoodapp/MainActivity.kt
+++ app/src/main/java/com/facom/sharehoodapp/MainActivity.kt
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnEsqueciMinhaSenha: Button
     private lateinit var btnCadastroUsuario: Button
+    private lateinit var edtTextLoginEmail: EditText
+    private lateinit var edtTextLoginSenha: EditText
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,8 @@ class MainActivity : AppCompatActivity() {
 
         btnEsqueciMinhaSenha = findViewById(R.id.btnLoginEsqueciMinhaSenha)
         btnCadastroUsuario = findViewById(R.id.btnLoginCadastrar)
+        edtTextLoginEmail = findViewById(R.id.edtTextLoginEmail)
+        edtTextLoginSenha = findViewById(R.id.edtTextLoginSenha)
     }
 
     fun goEsqueciMinhaSenha(view: View) {
@@ -27,5 +34,14 @@ class MainActivity : AppCompatActivity() {
     fun goCadastroUsuario(view: View) {
         var i = Intent(applicationContext, CadastroUsuarioActivity::class.java)
         startActivity(i)
+    }
+    fun entrar(view: View){
+        if(edtTextLoginEmail.text.toString().isEmpty()){
+            Toast.makeText(applicationContext, "Informe o email", Toast.LENGTH_LONG).show();
+        }
+        else if(edtTextLoginSenha.text.toString().isEmpty()){
+            Toast.makeText(applicationContext, "Informe a senha", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
