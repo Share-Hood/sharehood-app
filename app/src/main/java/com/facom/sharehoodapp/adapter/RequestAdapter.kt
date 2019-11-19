897d78e5f8f2d6e0aac76b68cc7978d0bc823090
Alison Moura <alison.oghino@gmail.com>
2019-11-18 21:22:34 -0400

    Fazendo ListView personalizada para listagem de pedidos

---
+++ app/src/main/java/com/facom/sharehoodapp/adapter/RequestAdapter.kt
@@ -0,0 +1,45 @@
+package com.facom.sharehoodapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.facom.sharehoodapp.R
+import com.facom.sharehoodapp.model.Request
+
+class RequestAdapter (private val context: Context,
+                      private val dataSource: ArrayList<Request>) : BaseAdapter() {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val rowView = inflater.inflate(R.layout.list_item_request, parent, false)
+        val txtViewListItemRequestNome = rowView.findViewById(R.id.txtViewListItemRequestNome) as TextView
+        val txtViewListItemRequestNomePedinte = rowView.findViewById(R.id.txtViewListItemRequestNomePedinte) as TextView
+        val txtViewListItemRequestDuracaoDias = rowView.findViewById(R.id.txtViewListItemRequestDuracaoDias) as TextView
+        val txtViewListItemRequestMotivo = rowView.findViewById(R.id.txtViewListItemRequestMotivo) as TextView
+
+        val request = getItem(position) as Request
+        txtViewListItemRequestNome.text = request.name
+        txtViewListItemRequestNomePedinte.text = request.user
+        txtViewListItemRequestDuracaoDias.text = "Por ${request.duration} dias"
+        txtViewListItemRequestMotivo.text = request.reason
+
+        return rowView
+    }
+
+    override fun getItem(position: Int): Any {
+        return dataSource[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+
+    private val inflater: LayoutInflater
+            = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+}
